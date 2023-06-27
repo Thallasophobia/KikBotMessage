8cf0ffc317d29eaf103b0b1e10699feaef1138c6
Thallasophobia <72714142+Thallasophobia@users.noreply.github.com>
2023-06-28 00:58:15 +0200

    kikbot.py

--- kikbot.py
+++ kikbot.py
@@ -8,19 +8,19 @@ kik = KikApi('your_bot_username', 'your_api_key')
 config = Configuration(webhook='https://your-webhook-url.com')
 kik.set_configuration(config)
 
-# Define the list of possible random messages. Create a "" string to add your messages fx "Hello",.
+# Define the list of possible random messages. Add your own messages here.
 messages = [
     "To anger a conservative, lie to him. To anger a liberal, tell him the truth!",
     "Fart?",
     "Spaghetti!",
     "Greetings from your Kik bot!",
-    "Trumo 2024",
-    "Sleepy joe",
-    "The modern conservative is engaged in one of man's oldest exercises in moral philosophy; that is, the search for a superior moral justification for selfishness.?"
+    "Trump 2024",
+    "Sleepy Joe",
+    "The modern conservative is engaged in one of man's oldest exercises in moral philosophy; that is, the search for a superior moral justification for selfishness.",
 ]
 
 # Define the interval and number of random messages
-interval_minutes = 60  # Change this to set the interval in seconds
+interval_minutes = 60  # Change this to set the interval in minutes
 num_messages = 1  # Change this to set the number of random messages to send
 
 # Function to send random messages
@@ -30,7 +30,22 @@ def send_random_messages():
         kik.send_messages([TextMessage(to='your_kik_username', body=message)])
         time.sleep(1)  # Add a short delay between messages
 
+# Event handler for incoming text messages
+@kik.on_text
+def handle_text_message(message):
+    # Process the incoming text message
+    # Send a response or perform necessary actions
+    pass
+
+# Event handler for incoming scan data
+@kik.on_scan_data
+def handle_scan_data(message):
+    # Process the incoming scan data
+    # Send a response or perform necessary actions
+    pass
+
 # Loop to send random messages at the specified interval
 while True:
     send_random_messages()
-    time.sleep(interval_minutes)
+    time.sleep(interval_minutes * 60)  # Convert minutes to seconds
+
